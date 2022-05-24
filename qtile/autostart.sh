cd24c65c1e5947595a8987fe55a12929e755e46a
BETAIDK <idankob@gmail.com>
2022-05-24 19:33:23 +0300

    mainm

---
+++ qtile/autostart.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+setxkbmap -option grp:alt_shift_toggle "us,il"
+feh --bg-scale ~/Pictures/Frame3.jpg
+picom &
---
+++ qtile/utils/settings.py
@@ -0,0 +1,12 @@
+# --==[ Settings ]==--
+
+# General
+font = 'SauceCodePro Nerd Font Medium'
+wallpaper = '~/Pictures/wallpapersden.com_zhongli-genshin-impact-new-art_2560x1440.jpg'
+
+# Weather
+location = {'Israel': 'Israel'}
+city = 'HAIFA, MX'
+
+# Color Schemes
+colorscheme = 'gruvbox_material'
